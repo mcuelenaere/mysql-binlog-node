c2b912c1a067f90fa929472ded99aa077630de56
Maurus Cuelenaere <mcuelenaere@gmail.com>
2022-08-25 18:52:13 +0200

    Get basic tests working again

--- test/test_binding.js
+++ test/test_binding.js
@@ -1,21 +1,27 @@
-const MysqlBinlog = require("../dist/binding.js");
+const { default: MysqlBinlog } = require("../dist/binding.js");
 const assert = require("assert");
 
 assert(MysqlBinlog, "The expected module is undefined");
 
 function testBasic()
 {
-    const instance = new MysqlBinlog("localhost", 3306, "root", "password", ['Assets']);
-    assert(instance.greet, "The expected method is not defined");
-    assert.strictEqual(instance.greet("kermit"), "mr-yeoman", "Unexpected value returned");
+    const instance = new MysqlBinlog({
+        hostname: "localhost",
+        port: 3306,
+        username: "root",
+        password: "password",
+        tableRegexes: ['Users'],
+    });
+    instance.close();
 }
 
 function testInvalidParams()
 {
     const instance = new MysqlBinlog();
+    instance.close();
 }
 
-assert.doesNotThrow(testBasic, undefined, "testBasic threw an expection");
+assert.throws(testBasic, undefined, "dial tcp [::1]:3306: connect: connection refused");
 assert.throws(testInvalidParams, undefined, "testInvalidParams didn't throw");
 
 console.log("Tests passed- everything looks OK!");
